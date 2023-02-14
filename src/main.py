624643ea0081a892ff6eba35bfd634f885cd7bb2
wilsvd <wilvandijkhuizen@gmail.com>
2023-02-14 19:14:58 +0000

    fix: add nltk imports for future users

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ from features.question_answer import QuestionAnswer
 from features.identity_management import IdentityManagement
 from utility.process_text import ProcessText
 
+# import nltk
+
+
 # DONE PROCESS
 small_talk = SmallTalk()
 q_a = QuestionAnswer()
@@ -10,28 +13,37 @@ id_manage = IdentityManagement()
 process_text = ProcessText()
 
 if __name__ == "__main__":
+    # nltk.download("stopwords")
+    # nltk.download('wordnet')
+    # nltk.download('universal_tagset')
+    # nltk.download('averaged_perceptron_tagger')
+    # nltk.download('punkt')
+    # nltk.download('omw-1.4')
+
     print("\nChatbot: Hi, I'm Steve. What is your name?\n")
     user_input = input(f"User: ").lower()
     user_name = id_manage.set_username(user_input)
     print(
-        f"Steve: Let me know at any time if you want to change your username {user_name} :)")
-    print("Steve: You can either chat with me or ask questions about:\n -- University, Youtube, Humanism, Geological History of Earth, Police, Infection, Hunting --\n")
+        f"Steve: Let me know at any time if you want to change your username {user_name} :)"
+    )
+    print(
+        "Steve: You can either chat with me or ask questions about:\n -- University, Youtube, Humanism, Geological History of Earth, Police, Infection, Hunting --\n"
+    )
     print("Steve: If you want to end the conversation at any time just say Bye \n")
 
     query = "TEMP STRING"
-    while (query):
+    while query:
         q_input = input(user_name + ": ")
-        query = process_text.preprocess_text(
-            text=q_input, type="lemmatisation")
+        query = process_text.preprocess_text(text=q_input, type="lemmatisation")
 
         response = q_a.answer_question(query)
-        if (response != "NOT FOUND"):
+        if response != "NOT FOUND":
             print(f"Steve: Here is the answer to your question --- {response}")
             continue
 
         response = id_manage.get_name_similarity(query)
-        if (response != "NOT FOUND"):
-            if (id_manage.is_name_change(query)):
+        if response != "NOT FOUND":
+            if id_manage.is_name_change(query):
                 print(f"Steve: {user_name}, please enter a new name")
                 user_input = input(user_name + ": ").lower()
                 user_name = id_manage.set_username(user_input)
